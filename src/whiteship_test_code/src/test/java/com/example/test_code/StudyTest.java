8be1f77d861d281ec4dbb1a1f44fdecabcd78167
soongjamm <kimstz0303@gmail.com>
2020-12-28 03:37:21 +0900

    애플리케이션을 테스트하는 다양한 방법: JUnit 소개/시작

---
+++ src/whiteship_test_code/src/test/java/com/example/test_code/StudyTest.java
@@ -0,0 +1,40 @@
+package com.example.test_code;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudyTest {
+
+    @Test
+    void create() {
+        Study study = new Study();
+        assertNotNull(study);
+        System.out.println("create");
+    }
+
+    @Test
+    @Disabled
+    void create1() {
+        System.out.println("create1");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("beforeAll");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("afterAll");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("beforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("afterEach");
+    }
+}
