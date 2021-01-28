0a77b1bb42431715c96826f1b3df45eed57605bd
soongjamm <kimstz0303@gmail.com>
2021-01-28 16:29:42 +0900

    스프링5 프로그래밍 입문: ch13.3 - 쿠키 사용하기

--- src/enter_spring5/src/main/java/chap13/src/main/java/controller/LoginController.java
+++ src/enter_spring5/src/main/java/chap13/src/main/java/controller/LoginController.java
@@ -2,12 +2,11 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import spring.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -21,12 +20,17 @@ public class LoginController {
     }
 
     @GetMapping
-    public String form(LoginCommand loginCommand) {
+    public String form(LoginCommand loginCommand,
+                       @CookieValue(value = "REMEMBER", required = false) Cookie rCookie) {
+        if(rCookie != null) {
+            loginCommand.setEmail(rCookie.getValue());
+            loginCommand.setRememberEmail(true);
+        }
         return "login/loginForm";
     }
 
     @PostMapping
-    public String submit(LoginCommand loginCommand, Errors errors, HttpSession session) {
+    public String submit(LoginCommand loginCommand, Errors errors, HttpSession session, HttpServletResponse response) {
         new LoginCommandValidator().validate(loginCommand, errors);
         if (errors.hasErrors()) {
             return "login/loginForm";
@@ -38,6 +42,16 @@ public class LoginController {
 
             // TODO 세션에 authInfo 저장해야 함
             session.setAttribute("authInfo", authInfo);
+
+            // 쿠키
+            Cookie rememberCookie = new Cookie("REMEMBER", loginCommand.getEmail());
+            rememberCookie.setPath("/");
+            if (loginCommand.isRememberEmail()) {
+                rememberCookie.setMaxAge(60 * 60 * 24 * 30);
+            } else {
+                rememberCookie.setMaxAge(0);
+            }
+            response.addCookie(rememberCookie);
 
             return "login/loginSuccess";
         } catch (WrongIdPasswordException e) {
--- src/enter_spring5/src/main/java/chap13/target/chap13-1.0-SNAPSHOT/WEB-INF/view/login/loginForm.jsp
+++ src/enter_spring5/src/main/java/chap13/target/chap13-1.0-SNAPSHOT/WEB-INF/view/login/loginForm.jsp
@@ -10,24 +10,29 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title><spring:message code="login.title" /></title>
+    <title><spring:message code="login.title"/></title>
 </head>
 <body>
-    <form:form modelAttribute="loginCommand">
-    <form:errors />
+<form:form modelAttribute="loginCommand">
+    <form:errors/>
     <p>
-        <label><spring:message code="email" />:</br>
-        <form:input path="email" />
-        <form:errors path="email" />
+        <label><spring:message code="email"/>:</br>
+            <form:input path="email"/>
+            <form:errors path="email"/>
         </label>
     </p>
     <p>
         <label><spring:message code="password"/>:</br>
-            <form:input path="password" />
-            <form:errors path="password" />
+            <form:input path="password"/>
+            <form:errors path="password"/>
+        </label>
+    </p>
+    <p>
+        <label><spring:message code="rememberEmail"/>
+            <form:checkbox path="rememberEmail"/>
         </label>
     </p>
     <input type="submit" value="<spring:message code="login.btn" />">
-    </form:form>
+</form:form>
 </body>
 </html>
