b27b333b20ffb723c7e1843cf6a46805b819b9f5
soongjamm <kimstz0303@gmail.com>
2021-01-28 18:34:36 +0900

    스프링5 프로그래밍 입문: ch14.2 - @Exceptionhandler를 이용한 메소드는 같은 컨트롤러 내에서 발생한 예외를 처리해준다.

--- src/enter_spring5/src/main/java/chap14/src/main/java/controller/MemberDetailController.java
+++ src/enter_spring5/src/main/java/chap14/src/main/java/controller/MemberDetailController.java
@@ -1,7 +1,9 @@
 package controller;
 
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import spring.Member;
@@ -25,5 +27,16 @@ public class MemberDetailController {
         }
         model.addAttribute("member", member);
         return "member/memberDetail";
+    }
+
+    @ExceptionHandler(TypeMismatchException.class)
+    public String handleTypeMismatchException() {
+        // 사용해서 로그 남기는 작업 등.. 필요한 작업 한다.
+        return "member/invalidId";
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public String handleMemberNotFoundException(){
+        return "member/noMember";
     }
 }
---
+++ src/enter_spring5/src/main/java/chap14/target/chap14-1.0-SNAPSHOT/WEB-INF/view/member/invalidId.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: soongjamm
+  Date: 2021/01/28
+  Time: 6:31 오후
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>에러</title>
+</head>
+<body>
+잘못된 요청입니다.
+</body>
+</html>
