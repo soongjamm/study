4b7b06e320f1f4c316bd29c4b698d1b61904a36e
soongjamm <kimstz0303@gmail.com>
2021-04-01 21:02:42 +0900

    스프링핵심원리 기본 도메인

---
+++ src/inflearn/kim_young_han/spring-core-principal-basic/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    MemberRepository memberRepository = new MemoryMemberRepository();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberRepository.save(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
