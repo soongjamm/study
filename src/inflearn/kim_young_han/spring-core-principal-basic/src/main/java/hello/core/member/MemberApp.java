faff0c1fd4394fc188bd5d668f37d7c02e864189
soongjamm <kimstz0303@gmail.com>
2021-04-02 11:49:08 +0900

    관심사의 분리로 DIP 원칙 준수

--- src/inflearn/kim_young_han/spring-core-principal-basic/src/main/java/hello/core/member/MemberApp.java
+++ src/inflearn/kim_young_han/spring-core-principal-basic/src/main/java/hello/core/member/MemberApp.java
@@ -1,14 +1,12 @@
 package hello.core.member;
 
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.AppConfig;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
--- src/inflearn/kim_young_han/spring-core-principal-basic/src/test/java/hello/core/order/OrderServiceTest.java
+++ src/inflearn/kim_young_han/spring-core-principal-basic/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,24 +1,30 @@
 package hello.core.order;
 
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
+import hello.core.AppConfig;
+import hello.core.member.*;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceTest {
 
-    MemberRepository memberRepository = new MemoryMemberRepository();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
-        memberRepository.save(member);
+        memberService.join(member);
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
