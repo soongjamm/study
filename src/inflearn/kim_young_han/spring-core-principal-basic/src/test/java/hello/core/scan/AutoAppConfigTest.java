bfe2fbd30271932aa339edaf604b4313c12fe2ac
soongjamm <kimstz0303@gmail.com>
2021-04-11 13:27:47 +0900

    스프링핵심원리: 필드주입을 권장하지 않는 이유

--- src/inflearn/kim_young_han/spring-core-principal-basic/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/inflearn/kim_young_han/spring-core-principal-basic/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -1,10 +1,18 @@
 package hello.core.scan;
 
 import hello.core.AutoAppConfig;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
 import hello.core.member.MemberService;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.scan.sample.OrderServiceConstructorInjection;
+import hello.core.scan.sample.OrderServiceFieldInjection;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AutoAppConfigTest {
 
@@ -13,5 +21,27 @@ public class AutoAppConfigTest {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
         MemberService memberService = ac.getBean(MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+    @Test
+    void fieldInjectionTest() {
+        // 테스트에서는 스프링을 사용하지 않고 순수 자바로만 테스트하게 될 수도 있다.
+        // 이 경우 field injection 을 해줄 수가 없다. '빈 컨테이너' 가 없기 때문이다.
+
+        // field injection 의 단점은 테스트시에 내가 원하는 의존성으로 교체(주입)해줄 수가 없다. -> NPE 가 터진다.
+        OrderServiceFieldInjection orderService1 = new OrderServiceFieldInjection();
+        assertThrows(NullPointerException.class,
+                () -> orderService1.createOrder(1L, "item", 10000)
+        );
+
+        // 그러나 생성자 or Setter 주입을 하면 테스트시에도 이렇게 의존성을 변경해줄 수 있다.
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository(); // 내부에서 member 때문에 npe 터지는것 방지
+        memberRepository.save(new Member(1L, "item", Grade.BASIC));
+
+        OrderServiceConstructorInjection orderService2 =
+                new OrderServiceConstructorInjection(memberRepository, new RateDiscountPolicy());
+        assertDoesNotThrow(
+                () -> orderService2.createOrder(1L, "item", 10000)
+        );
     }
 }
--- src/inflearn/kim_young_han/spring-core-principal-basic/src/test/java/hello/core/scan/sample/OrderServiceFieldInjection.java
+++ src/inflearn/kim_young_han/spring-core-principal-basic/src/test/java/hello/core/scan/sample/OrderServiceFieldInjection.java
@@ -1,28 +1,23 @@
-package hello.core.order;
+package hello.core.scan.sample;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OrderServiceImpl implements OrderService {
-
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+public class OrderServiceFieldInjection implements OrderService {
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    private MemberRepository memberRepository;
+    @Autowired
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
-        System.out.println((memberRepository==null) + " " + (discountPolicy==null));
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
         return new Order(memberId, itemName, itemPrice, discountPrice);
