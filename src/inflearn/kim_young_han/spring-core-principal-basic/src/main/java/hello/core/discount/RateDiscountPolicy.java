3226b4b8f61318770ea3e2e5f24e2f5667e76767
soongjamm <kimstz0303@gmail.com>
2021-04-25 13:11:50 +0900

    inflearn:spring-core - singleton, prototype

--- src/inflearn/kim_young_han/spring-core-principal-basic/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/inflearn/kim_young_han/spring-core-principal-basic/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,8 +2,10 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary // Autowired 할 빈이 2개 이상일 때 @Primary를 가져온다. (@Qualifier 지정 안했을 경우)
 @Component
 public class RateDiscountPolicy implements DiscountPolicy {
 
---
+++ src/inflearn/kim_young_han/spring-core-principal-basic/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,40 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+
+    @Test
+    void PrototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        @PostConstruct // not working
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy // not working
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
