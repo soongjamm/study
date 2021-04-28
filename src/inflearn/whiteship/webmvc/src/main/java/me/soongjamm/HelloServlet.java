3b357eb3318c7812dbec157bf366ae8ef91d6497
soongjamm <kimstz0303@gmail.com>
2021-04-29 01:10:08 +0900

    inflearn whiteship - webmvc init

---
+++ src/inflearn/whiteship/webmvc/src/main/java/me/soongjamm/HelloServlet.java
@@ -0,0 +1,28 @@
+package me.soongjamm;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
+        resp.getWriter().println("<html>");
+        resp.getWriter().println("<head>");
+        resp.getWriter().println("<body>");
+        resp.getWriter().println("<h1>Hello Servlet</h1>");
+        resp.getWriter().println("</body>");
+        resp.getWriter().println("</head>");
+        resp.getWriter().println("</html>");
+    }
+}
