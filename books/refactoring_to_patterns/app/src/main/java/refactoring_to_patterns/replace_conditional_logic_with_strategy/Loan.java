967087f042f9caf25346f01203a5ee1b175791f5
seungtae.kim <seungtae.kim@yanolja.com>
2022-04-05 22:57:19 +0900

    step2 introduce strategy

--- books/refactoring_to_patterns/app/src/main/java/refactoring_to_patterns/replace_conditional_logic_with_strategy/Loan.java
+++ books/refactoring_to_patterns/app/src/main/java/refactoring_to_patterns/replace_conditional_logic_with_strategy/Loan.java
@@ -9,47 +9,57 @@ public class Loan {
     private double outstanding;
     private double commitment;
     private Date expiry;
-    private Object maturity;
+    private Date maturity;
     private long weightedAverage;
     private Date today;
     private Date start;
-    private Object riskRating;
+    private int riskRating;
     private Iterable<Payment> payments;
+    private CapitalStrategy capitalStrategy;
 
-    public double capital() { // 대출의 종류를 결정짓고 계산이 진행되는데, 어떤 종류의 대출인지알아보기가 어렵다.
-        if (expiry == null && maturity != null) {
-            return commitment * duration() * riskFactor();
-        }
-        if (expiry != null && maturity == null) {
-            if (getUnusedPercentage() != 1.0) {
-                return commitment * getUnusedPercentage() * duration() * riskFactor();
-            } else {
-                return (outstandingRiskAmount() * duration() * riskFactor())
-                        + (unusedRiskAmount() * duration() * unusedRiskFactor());
-            }
-        }
-        return 0.0;
+    public static Loan newTermLoan(double commitment, Date start, Date maturity, int riskRating) {
+        return new Loan(commitment, commitment, start, null, maturity, riskRating, new CapitalStrategy());
     }
 
-    private double getUnusedPercentage() {
+    public static Loan newRevolver(double commitment, Date start, Date expiry, int riskRating) {
+        return new Loan(commitment, 0, start, expiry, null, riskRating, new CapitalStrategy())
+    }
+
+    public static Loan newAdvisedLine(double commitment, Date start, Date expiry, int riskRating) {
+        if (riskRating > 3) return null;
+        Loan advisedLine = new Loan(commitment, 0, start, expiry, null, riskRating, new CapitalStrategy());
+        advisedLine.setUnusedPercentage(0.1);
+        return advisedLine;
+    }
+
+    private void setUnusedPercentage(double v) {
+
+    }
+
+    public Loan(double outstanding, double commitment, Date expiry, Date maturity, Date start, int riskRating, CapitalStrategy capitalStrategy) {
+        this.capitalStrategy = capitalStrategy;
+        this.outstanding = outstanding;
+        this.commitment = commitment;
+        this.expiry = expiry;
+        this.maturity = maturity;
+        this.start = start;
+        this.riskRating = riskRating;
+    }
+
+    public double capital() { // 대출의 종류를 결정짓고 계산이 진행되는데, 어떤 종류의 대출인지알아보기가 어렵다.
+        return capitalStrategy.capital(this);
+    }
+
+    public double getUnusedPercentage() {
         return 0;
     }
 
-    private double outstandingRiskAmount() {
-        return outstanding;
-    }
-
-    private double unusedRiskAmount() {
+    public double unusedRiskAmount() {
         return (commitment - outstanding);
     }
 
     public double duration() {
-        if (expiry == null && maturity != null) {
-            return weightedAverageDuration();
-        } else if (expiry != null && maturity == null) {
-            return yearsTo(expiry);
-        }
-        return 0.0;
+        return capitalStrategy.duration(this);
     }
 
     private double weightedAverageDuration() {
@@ -75,21 +85,13 @@ public class Loan {
         return ((endDate.getTime() - beginDate.getTime()) / MILLIS_PER_DAY) / DAYS_PER_YEAR;
     }
 
-    private double riskFactor() {
-        return RiskFactor.getFactors().forRating(riskRating);
-    }
-
-    private double unusedRiskFactor() {
-        return UnusedRiskFactors.getFactors().forRating(riskRating);
-    }
-
-    private static class Factors {
+    public static class Factors {
         public double forRating(Object riskRating) {
             return 0;
         }
     }
 
-    private class Payment implements Iterable<Payment> {
+    public class Payment implements Iterable<Payment> {
 
         @Override
         public Iterator<Payment> iterator() {
@@ -105,15 +107,51 @@ public class Loan {
         }
     }
 
-    private static class RiskFactor {
+    public static class RiskFactor {
         public static Factors getFactors() {
             return null;
         }
     }
 
-    private static class UnusedRiskFactors {
+    public static class UnusedRiskFactors {
         public static Factors getFactors() {
             return null;
         }
+    }
+
+    double getOutstanding() {
+        return outstanding;
+    }
+
+    double getCommitment() {
+        return commitment;
+    }
+
+    Date getExpiry() {
+        return expiry;
+    }
+
+    Date getMaturity() {
+        return maturity;
+    }
+
+    long getWeightedAverage() {
+        return weightedAverage;
+    }
+
+    Date getToday() {
+        return today;
+    }
+
+    Date getStart() {
+        return start;
+    }
+
+    Object getRiskRating() {
+        return riskRating;
+    }
+
+    Iterable<Payment> getPayments() {
+        return payments;
     }
 }
---
+++ books/refactoring_to_patterns/app/src/test/groovy/refactoring_to_patterns/replace_conditional_logic_with_strategy/CapitalCalculationTests.java
@@ -0,0 +1,22 @@
+package refactoring_to_patterns.replace_conditional_logic_with_strategy;
+
+import junit.framework.TestCase;
+
+import java.util.Date;
+
+public class CapitalCalculationTests extends TestCase {
+//    public void testTermLoanSamePayments() {
+//        Date start = november(20, 2003);
+//        Date maturity = november(20, 2006);
+//        Loan termLoan = Loan.newTermLoan(LOAN_AMOUNT, start, maturity, HIGH_RISK_RATING);
+//        termLoan.payment(1000.00, november(20,2004));
+//        termLoan.payment(1000.00, november(20,2004));
+//        termLoan.payment(1000.00, november(20,2004));
+//        assertEquals("duration", 2.0, termLoan.duration(), TWO_DIGIT_PRECISION);
+//        assertEquals("capital", 210.0, termLoan.duration(), TWO_DIGIT_PRECISION);
+//    }
+//
+//    private Date november(int day, int year) {
+//        return new Date(year, 11, day);
+//    }
+}
