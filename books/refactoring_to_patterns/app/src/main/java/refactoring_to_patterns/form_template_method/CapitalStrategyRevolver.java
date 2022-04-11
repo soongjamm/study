ed874a884f716ffbdcaa7226499b03196a7246cb
seungtae.kim <seungtae.kim@yanolja.com>
2022-04-11 21:47:23 +0900

    step 3 override with super method

--- books/refactoring_to_patterns/app/src/main/java/refactoring_to_patterns/form_template_method/CapitalStrategyRevolver.java
+++ books/refactoring_to_patterns/app/src/main/java/refactoring_to_patterns/form_template_method/CapitalStrategyRevolver.java
@@ -2,12 +2,20 @@ package refactoring_to_patterns.form_template_method;
 
 public class CapitalStrategyRevolver extends CapitalStrategy {
     public double capital(Loan loan) {
-        return (loan.getOutstanding() * duration(loan) * riskFactorFor(loan))
-                + (loan.unusedRiskAmount() * duration(loan) * unusedRiskFactorFor(loan));
+        return super.capital(loan) + unusedCapital(loan);
+    }
+
+    private double unusedCapital(Loan loan) {
+        return loan.unusedRiskAmount() * duration(loan) * unusedRiskFactorFor(loan);
     }
 
     public double duration(Loan loan) {
         return 0.0;
+    }
+
+    @Override
+    protected double riskAmountFor(Loan loan) {
+        return loan.getOutstanding();
     }
 
     private double unusedRiskFactorFor(Loan loan) {
