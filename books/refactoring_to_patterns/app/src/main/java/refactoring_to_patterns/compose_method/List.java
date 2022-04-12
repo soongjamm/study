a77e5e3bf910f56167cfee4ed40ac3553bb4777b
soongjamm <kimstz0303@gmail.com>
2022-04-12 20:06:03 +0900

    step2 early return rather than cover the whole thing

--- books/refactoring_to_patterns/app/src/main/java/refactoring_to_patterns/compose_method/List.java
+++ books/refactoring_to_patterns/app/src/main/java/refactoring_to_patterns/compose_method/List.java
@@ -5,19 +5,21 @@ public class List {
     private Object[] elements;
     private int newSize;
     private int size;
+    private boolean readonly;
 
     public void add(Object element) {
-        boolean readonly = false;
-        if (!readonly) {
-            newSize = size + 1;
-            if (newSize > elements.length) {
-                Object[] newElements = new Object[elements.length + 10];
-                for (int i = 0; i < size; i++) {
-                    newElements[i] = elements[i];
-                }
-                elements = newElements;
-            }
-            elements[size++] = element;
+        if(readonly) {
+            return;
         }
+
+        newSize = size + 1;
+        if (newSize > elements.length) {
+            Object[] newElements = new Object[elements.length + 10];
+            for (int i = 0; i < size; i++) {
+                newElements[i] = elements[i];
+            }
+            elements = newElements;
+        }
+        elements[size++] = element;
     }
 }
