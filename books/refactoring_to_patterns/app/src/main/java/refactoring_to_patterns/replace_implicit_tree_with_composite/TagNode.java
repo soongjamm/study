125f7014451c19ece5d0487da3ff1e19938046f4
soongjamm <kimstz0303@gmail.com>
2022-04-12 22:02:53 +0900

    step2 Create implicit terminating node by tdd

---
+++ books/refactoring_to_patterns/app/src/main/java/refactoring_to_patterns/replace_implicit_tree_with_composite/TagNode.java
@@ -0,0 +1,33 @@
+package refactoring_to_patterns.replace_implicit_tree_with_composite;
+
+public class TagNode {
+    private String name = "";
+    private String value = "";
+    private StringBuffer attributes;
+
+    public TagNode(String name) {
+        this.name = name;
+        attributes = new StringBuffer("");
+    }
+
+    public void addAttribute(String attribute, String value) {
+        attributes.append(" ");
+        attributes.append(attribute);
+        attributes.append("='");
+        attributes.append(value);
+        attributes.append("'");
+    }
+
+    public void addValue(String value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        String result;
+        result =
+                "<" + name + attributes + ">" +
+                        value +
+                        "</" + name + ">";
+        return result;
+    }
+}
