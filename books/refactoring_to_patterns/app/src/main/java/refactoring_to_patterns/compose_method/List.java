08b777be47b583414be5844de7cb554cfe38eec0
soongjamm <kimstz0303@gmail.com>
2022-04-12 20:12:04 +0900

    step4 'method extract' to adjust abstraction level

--- books/refactoring_to_patterns/app/src/main/java/refactoring_to_patterns/compose_method/List.java
+++ books/refactoring_to_patterns/app/src/main/java/refactoring_to_patterns/compose_method/List.java
@@ -4,23 +4,33 @@ public class List {
 
     private static final int GROWTH_INCREMENT = 10;
     private Object[] elements;
-    private int newSize;
     private int size;
     private boolean readonly;
 
     public void add(Object element) {
-        if(readonly) {
+        if (readonly) {
             return;
         }
 
-        newSize = size + 1;
-        if (newSize > elements.length) {
-            Object[] newElements = new Object[elements.length + GROWTH_INCREMENT];
-            for (int i = 0; i < size; i++) {
-                newElements[i] = elements[i];
-            }
-            elements = newElements;
+        if (atCapacity()) {
+            grow();
         }
+        addElement(element);
+    }
+
+    private void addElement(Object element) {
         elements[size++] = element;
+    }
+
+    private void grow() {
+        Object[] newElements = new Object[elements.length + GROWTH_INCREMENT];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[i];
+        }
+        elements = newElements;
+    }
+
+    private boolean atCapacity() {
+        return (size + 1) > elements.length;
     }
 }
