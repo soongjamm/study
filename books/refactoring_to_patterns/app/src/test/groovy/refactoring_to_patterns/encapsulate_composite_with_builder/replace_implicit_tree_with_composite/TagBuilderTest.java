863eb2d5941ef45759c71ada23f0621b7f739d57
soongjamm <kimstz0303@gmail.com>
2022-04-26 00:19:18 +0900

    step2 more complex test

--- books/refactoring_to_patterns/app/src/test/groovy/refactoring_to_patterns/encapsulate_composite_with_builder/replace_implicit_tree_with_composite/TagBuilderTest.java
+++ books/refactoring_to_patterns/app/src/test/groovy/refactoring_to_patterns/encapsulate_composite_with_builder/replace_implicit_tree_with_composite/TagBuilderTest.java
@@ -3,6 +3,8 @@ package refactoring_to_patterns.encapsulate_composite_with_builder.replace_impli
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.*;
+
 public class TagBuilderTest {
 
     @Test
@@ -18,7 +20,7 @@ public class TagBuilderTest {
         String expectedXml =
                 "<flavors>" +
                         "<flavor/>" +
-                "</flavors>";
+                        "</flavors>";
         TagBuilder tagBuilder = new TagBuilder("flavors");
         tagBuilder.addChild("flavor");
         String actualXml = tagBuilder.toXml();
@@ -26,4 +28,24 @@ public class TagBuilderTest {
         Assertions.assertEquals(expectedXml, actualXml);
     }
 
+
+    @Test
+    void testBuildChildrenOfChildren() {
+        String expected =
+                "<flavors>" +
+                    "<flavor>" +
+                        "<requirements>" +
+                            "<requirement/>" +
+                        "</requirements>" +
+                    "</flavor>" +
+                "</flavors>";
+
+        TagBuilder builder = new TagBuilder("flavors");
+        builder.addChild("flavor");
+        builder.addChild("requirements");
+        builder.addChild("requirement");
+        String actualXml = builder.toXml();
+
+        assertEquals(expected, actualXml);
+    }
 }
